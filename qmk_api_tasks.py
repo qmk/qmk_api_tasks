60a1ad83134b3bedf1d2e8733adeff46824a46eb
Zach White <skullydazed@gmail.com>
2020-12-21 22:35:02 -0800

    change api_tasks to use compile_json and fetch default keymaps

--- qmk_api_tasks.py
+++ qmk_api_tasks.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+import random
 import threading
 from datetime import datetime
 from os import environ
@@ -6,18 +7,23 @@ from time import sleep, strftime, time
 from traceback import print_exc
 from wsgiref.simple_server import make_server, WSGIRequestHandler
 
+import requests
+
 import qmk_redis
 from cleanup_storage import cleanup_storage
 from qmk_commands import discord_msg
-from qmk_compiler import compile_firmware
+from qmk_compiler import compile_json
 from update_kb_redis import update_kb_redis
 
 environ['S3_ACCESS_KEY'] = environ.get('S3_ACCESS_KEY', 'minio_dev')
 environ['S3_SECRET_KEY'] = environ.get('S3_SECRET_KEY', 'minio_dev_secret')
+DEBUG = int(environ.get('DEBUG', 1))  # Turn on/up debugging
 COMPILE_TIMEOUT = int(environ.get('COMPILE_TIMEOUT', 600))  # 10 minutes, how long we wait for a specific board to compile
 ERROR_LOG_NAG = environ.get('ERROR_LOG_NAG', 'yes') == 'yes'  # When 'yes', send a message to discord about how long the error log is.
 ERROR_LOG_URL = environ.get('ERROR_LOG_URL', 'http://api.qmk.fm/v1/keyboards/error_log')  # The URL for the error_log
 ERROR_PAGE_URL = environ.get('ERROR_PAGE_URL', 'https://yanfali.github.io/qmk_error_page/')  # The URL to the keyboard status page
+QMK_JSON_URL = environ.get('QMK_JSON_URL', 'https://keyboards.qmk.fm/v1')  # The URL for the QMK JSON data
+KEYMAP_JSON_URL = environ.get('KEYMAP_JSON_URL', 'https://raw.githubusercontent.com/qmk/qmk_configurator/master/public/keymaps')  # The URL for default keymaps
 JOB_QUEUE_THRESHOLD = int(environ.get('JOB_QUEUE_THRESHOLD', 1))  # When there are more than this many jobs in the queue we don't compile anything
 JOB_QUEUE_WAIT = int(environ.get('JOB_QUEUE_WAIT', 300))  # 5 Minutes, How long to wait when the job queue is too large
 JOB_QUEUE_TOO_LONG = int(environ.get('JOB_QUEUE_TOO_LONG', 1800))  # 30 Minutes, How long it's been since the last compile before we warn it's been too long
@@ -32,6 +38,7 @@ S3_CLEANUP_PERIOD = int(environ.get('S3_CLEANUP_PERIOD', 900))  # 15 Minutes, ho
 QMK_UPDATE_TIMEOUT = int(environ.get('QMK_UPDATE_TIMEOUT', 600))  # 10 Minutes, how long we wait for the qmk_firmware update to run
 QUEUE_TIMEOUT = int(environ.get('QUEUE_TIMEOUT', 3600))  # 1 Hour, how long we wait for qmk_firmware_update and s3_cleanup to queue
 BUILD_STATUS_TIMEOUT = int(environ.get('BUILD_STATUS_TIMEOUT', 86400 * 7))  # 1 week, how old configurator_build_status entries should be to get removed
+HTTP_TIMEOUT = int(environ.get('HTTP_TIMEOUT', 5))  # 5 seconds, how long to wait for HTTP calls
 TIME_FORMAT = environ.get('TIME_FORMAT', '%Y-%m-%d %H:%M:%S %z')
 
 # Status tracking variables
@@ -51,6 +58,22 @@ status = {
 class NoLoggingWSGIRequestHandler(WSGIRequestHandler):
     def log_message(self, format, *args):
         pass
+
+
+def fetch_json(url):
+    """Gets the JSON from a url.
+    """
+    if DEBUG:
+        print(f'fetch_json({url})')
+
+    response = requests.get(url, timeout=HTTP_TIMEOUT)
+    response.encoding='utf-8-sig'
+
+    if response.status_code == 200:
+        return response.json()
+
+    print(f'ERROR: {url} returned {response.status_code}: {response.text}')
+    return {}
 
 
 def current_status(i):
@@ -212,7 +235,14 @@ class TaskThread(threading.Thread):
         while True:
             good_boards = 0
             bad_boards = 0
-            keyboard_list = qmk_redis.get('qmk_api_keyboards')
+            keyboard_list_url = f'{QMK_JSON_URL}/keyboard_list.json'
+            keyboard_list = fetch_json(keyboard_list_url).get('keyboards')
+
+            if not keyboard_list:
+                print('Could not fetch keyboard list from %s! Running periodic_tasks() then sleeping %s seconds...' % (KEYBOARD_LIST_URL, COMPILE_TIMEOUT))
+                periodic_tasks()
+                sleep(COMPILE_TIMEOUT)
+                continue
 
             # If we stopped at a known keyboard restart from there
             if last_stop in keyboard_list:
@@ -241,80 +271,91 @@ class TaskThread(threading.Thread):
                             message = 'Compile queue too large (%s) since %s' % (len(qmk_redis.rq.jobs), job_queue_last_compile.isoformat())
                             discord_msg(level, message)
 
-                    # Cycle through each layout for this keyboard and build it
+                    # Find or generate a default keymap for this keyboard.
                     qmk_redis.set('qmk_api_tasks_current_keyboard', keyboard)
                     layout_results = {}
-                    metadata = qmk_redis.get('qmk_api_kb_%s' % (keyboard))
-                    if not metadata['layouts']:
+                    metadata_url = f'{QMK_JSON_URL}/keyboards/{keyboard}/info.json'
+                    metadata = fetch_json(metadata_url).get('keyboards', {}).get(keyboard)
+
+                    if not metadata:
+                        print(f'No metadata, skipping {keyboard}.')
+
+                    if metadata.get('keymaps') and 'default' in metadata['keymaps']:
+                        keymap = fetch_json(metadata['keymaps']['default']['url'])
+                    else:
+                        keymap_url = f'{KEYMAP_JSON_URL}/{keyboard[0]}/{keyboard.replace("/", "_")}_default.json'
+                        keymap = fetch_json(keymap_url)
+
+                        if not keymap:
+                            # Fall back to building an empty keymap
+                            if not metadata.get('layouts'):
+                                layout_macro = random.choice(metadata['layouts'])
+                                keymap = {
+                                    'keyboard': keyboard,
+                                    'keymap': 'qmk_api_tasks',
+                                    'layout': layout_macro,
+                                    'layers': [
+                                        list(map(lambda x: 'KC_NO', metadata['layouts'][layout_macro]['layout'])),
+                                        list(map(lambda x: 'KC_TRNS', metadata['layouts'][layout_macro]['layout']))
+                                    ]
+                                }
+
+                    if 'layout' not in keymap:
+                        keymap['layout'] = 'Unknown'
+                    if 'keymap' not in keymap:
+                        keymap['keymap'] = 'default'
+
+                    # Enqueue the job
+                    print('***', strftime(TIME_FORMAT))
+                    print('Beginning test compile for %s, layout %s' % (keyboard, keymap['layout']))
+                    job = qmk_redis.enqueue(compile_json, COMPILE_TIMEOUT, keymap, None, False)
+                    print('Successfully enqueued, polling every 2 seconds...')
+
+                    # Wait for the job to start running
+                    if not wait_for_job_start(job):
+                        print('Waited %s seconds for %s to start! Giving up at %s!' % (S3_CLEANUP_TIMEOUT, keyboard, strftime(TIME_FORMAT)))
+                        if MSG_ON_BAD_COMPILE:
+                            discord_msg('warning', 'Keyboard %s waited in queue longer than %s seconds! Queue length %s!' % (keyboard, S3_CLEANUP_TIMEOUT, len(qmk_redis.rq.jobs)))
+
+                    else:
+                        timeout = time() + COMPILE_TIMEOUT + 5
+                        while job.get_status() == 'started':
+                            if time() > timeout:
+                                print('Compile timeout reached after %s seconds, giving up on this job.' % (COMPILE_TIMEOUT))
+                                layout_results[keyboard] = {'result': False, 'reason': '**%s**: Compile timeout reached.' % keymap['layout']}
+                                break
+                            sleep(2)
+
+                    # Check over the job results
+                    if job.result and job.result['returncode'] == 0:
+                        print('Compile job completed successfully!')
+                        good_boards += 1
+                        qmk_redis.set('qmk_good_boards', good_boards)
+                        configurator_build_status[keyboard] = {'works': True, 'last_tested': int(time()), 'message': job.result['output']}
+                        keyboards_tested[keyboard] = True  # FIXME: Remove this when it's no longer used
+                        if keyboard in failed_keyboards:
+                            del failed_keyboards[keyboard]  # FIXME: Remove this when it's no longer used
+                        layout_results[keyboard] = {'result': True, 'reason': keyboard + ' works in configurator.'}
+                    else:
+                        if job.result:
+                            output = job.result['output']
+                            print('Could not compile %s, layout %s, return code %s' % (keyboard, keymap['layout'], job.result['returncode']))
+                            print(output)
+                            layout_results[keyboard] = {'result': False, 'reason': '**%s** does not work in configurator.' % keymap['layout']}
+                        else:
+                            output = 'Job took longer than %s seconds, giving up!' % COMPILE_TIMEOUT
+                            layout_results[keyboard] = {'result': False, 'reason': '**%s**: Compile timeout reached.' % keymap['layout']}
+
                         bad_boards += 1
                         qmk_redis.set('qmk_bad_boards', bad_boards)
-                        keyboards_tested[keyboard + '/[NO_LAYOUTS]'] = False  # FIXME: Remove when no longer used
-                        failed_keyboards[keyboard + '/[NO_LAYOUTS]'] = {'severity': 'error', 'message': '%s: No layouts defined.' % keyboard}  # FIXME: Remove when no longer used
-                        configurator_build_status[keyboard + '/[NO_LAYOUTS]'] = {'works': False, 'last_tested': int(time()), 'message': '%s: No Layouts defined.' % keyboard}
-                        continue
+                        configurator_build_status[keyboard] = {'works': False, 'last_tested': int(time()), 'message': output}
+                        keyboards_tested[keyboard] = False  # FIXME: Remove this when it's no longer used
+                        failed_keyboards[keyboard] = {'severity': 'error', 'message': output}  # FIXME: Remove this when it's no longer used
 
-                    for layout_macro in list(metadata['layouts']):
-                        # Skip KEYMAP as it's deprecated
-                        if 'KEYMAP' in layout_macro:
-                            continue
-
-                        # Build a layout
-                        keyboard_layout_name = '/'.join((keyboard, layout_macro))
-                        layers = [list(map(lambda x: 'KC_NO', metadata['layouts'][layout_macro]['layout']))]
-                        layers.append(list(map(lambda x: 'KC_TRNS', layers[0])))
-
-                        # Enqueue the job
-                        print('***', strftime(TIME_FORMAT))
-                        print('Beginning test compile for %s, layout %s' % (keyboard, layout_macro))
-                        args = (keyboard, 'qmk_api_tasks_test_compile', layout_macro, layers)
-                        job = qmk_redis.enqueue(compile_firmware, COMPILE_TIMEOUT, *args)
-                        print('Successfully enqueued, polling every 2 seconds...')
-
-                        # Wait for the job to start running
-                        if not wait_for_job_start(job):
-                            print('Waited %s seconds for %s/%s to start! Giving up at %s!' % (S3_CLEANUP_TIMEOUT, keyboard, layout_macro, strftime(TIME_FORMAT)))
-                            if MSG_ON_BAD_COMPILE:
-                                discord_msg('warning', 'Keyboard %s/%s waited in queue longer than %s seconds! Queue length %s!' % (keyboard, layout_macro, S3_CLEANUP_TIMEOUT, len(qmk_redis.rq.jobs)))
-
-                        else:
-                            timeout = time() + COMPILE_TIMEOUT + 5
-                            while job.get_status() == 'started':
-                                if time() > timeout:
-                                    print('Compile timeout reached after %s seconds, giving up on this job.' % (COMPILE_TIMEOUT))
-                                    layout_results[keyboard_layout_name] = {'result': False, 'reason': '**%s**: Compile timeout reached.' % layout_macro}
-                                    break
-                                sleep(2)
-
-                        # Check over the job results
-                        if job.result and job.result['returncode'] == 0:
-                            print('Compile job completed successfully!')
-                            good_boards += 1
-                            qmk_redis.set('qmk_good_boards', good_boards)
-                            configurator_build_status[keyboard_layout_name] = {'works': True, 'last_tested': int(time()), 'message': job.result['output']}
-                            keyboards_tested[keyboard_layout_name] = True  # FIXME: Remove this when it's no longer used
-                            if keyboard_layout_name in failed_keyboards:
-                                del failed_keyboards[keyboard_layout_name]  # FIXME: Remove this when it's no longer used
-                            layout_results[keyboard_layout_name] = {'result': True, 'reason': layout_macro + ' works in configurator.'}
-                        else:
-                            if job.result:
-                                output = job.result['output']
-                                print('Could not compile %s, layout %s, return code %s' % (keyboard, layout_macro, job.result['returncode']))
-                                print(output)
-                                layout_results[keyboard_layout_name] = {'result': False, 'reason': '**%s** does not work in configurator.' % layout_macro}
-                            else:
-                                output = 'Job took longer than %s seconds, giving up!' % COMPILE_TIMEOUT
-                                layout_results[keyboard_layout_name] = {'result': False, 'reason': '**%s**: Compile timeout reached.' % layout_macro}
-
-                            bad_boards += 1
-                            qmk_redis.set('qmk_bad_boards', bad_boards)
-                            configurator_build_status[keyboard_layout_name] = {'works': False, 'last_tested': int(time()), 'message': output}
-                            keyboards_tested[keyboard_layout_name] = False  # FIXME: Remove this when it's no longer used
-                            failed_keyboards[keyboard_layout_name] = {'severity': 'error', 'message': output}  # FIXME: Remove this when it's no longer used
-
-                        # Write our current progress to redis
-                        qmk_redis.set('qmk_api_configurator_status', configurator_build_status)
-                        qmk_redis.set('qmk_api_keyboards_tested', keyboards_tested)
-                        qmk_redis.set('qmk_api_keyboards_failed', failed_keyboards)
+                    # Write our current progress to redis
+                    qmk_redis.set('qmk_api_configurator_status', configurator_build_status)
+                    qmk_redis.set('qmk_api_keyboards_tested', keyboards_tested)
+                    qmk_redis.set('qmk_api_keyboards_failed', failed_keyboards)
 
                     # Report this keyboard to discord
                     failed_layout = False
@@ -342,10 +383,10 @@ class TaskThread(threading.Thread):
             # Remove stale build status entries
             print('***', strftime(TIME_FORMAT))
             print('Checking configurator_build_status for stale entries.')
-            for keyboard_layout_name in list(configurator_build_status):
-                if time() - configurator_build_status[keyboard_layout_name]['last_tested'] > BUILD_STATUS_TIMEOUT:
-                    print('Removing stale entry %s because it is %s seconds old' % (keyboard_layout_name, configurator_build_status[keyboard_layout_name]['last_tested']))
-                    del configurator_build_status[keyboard_layout_name]
+            for keyboard in list(configurator_build_status):
+                if time() - configurator_build_status[keyboard]['last_tested'] > BUILD_STATUS_TIMEOUT:
+                    print('Removing stale entry %s because it is %s seconds old' % (keyboard, configurator_build_status[keyboard]['last_tested']))
+                    del configurator_build_status[keyboard]
 
             # Notify discord that we've completed a circuit of the keyboards
             if MSG_ON_LOOP_COMPLETION:
@@ -374,9 +415,9 @@ class TaskThread(threading.Thread):
 
                     message = """We've completed a round of testing!
 
-Working: %s the last round, for a total of %s working keyboard/layout combinations.
+Working: %s the last round, for a total of %s working keyboards.
 
-Non-working: %s the last round, for a total of %s non-working keyboard/layout combinations.
+Non-working: %s the last round, for a total of %s non-working keyboards.
 
 Check out the details here: <%s>"""
                     discord_msg('info', message % (good_difference, good_boards, bad_difference, bad_boards, ERROR_PAGE_URL))
